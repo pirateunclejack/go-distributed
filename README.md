3df08a717cab2be372b4a2b76ed84c0ca9cb3205
pirate <17961436+pirateunclejack@users.noreply.github.com>
2023-05-10 22:33:00 +0800

    2.5 service registartion register a service

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ## 2 service registration
 
 ### 2.1 custom log logic
+
+### 2.2 excutable log service
+
+### 2.3 logic to register service
+
+### 2.4 excutable service registration web service
+
+### 2.5 register a service
--- service/service.go
+++ service/service.go
@@ -2,19 +2,24 @@ package service
 
 import (
 	"context"
+	"distributed/registry"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func Start(ctx context.Context, serviceName, host, port string, registerHandersFunc func()) (context.Context, error) {
+func Start(ctx context.Context, host, port string, reg registry.Registration, registerHandersFunc func()) (context.Context, error) {
 	registerHandersFunc()
-	ctx = startService(ctx, serviceName, host, port)
+	ctx = startService(ctx, reg.ServiceName, host, port)
+	err := registry.RegisterService(reg)
+	if err != nil {
+		return ctx, err
+	}
 
 	return ctx, nil
 }
 
-func startService(ctx context.Context, serviceName, host, port string) context.Context {
+func startService(ctx context.Context, serviceName registry.ServiceName, host, port string) context.Context {
 	ctx, cancel := context.WithCancel(ctx)
 	var srv http.Server
 	srv.Addr = ":" + port
