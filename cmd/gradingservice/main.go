9f9d2d1a3648e01af5f91e0a0ff70d8edc39a835
Jack Cheng <17961436+pirateunclejack@users.noreply.github.com>
2023-05-25 01:22:33 +0800

    3.3 service discovery (#9)
    * 3.3 service discovery 1
    
    * 3.3 service discovery 2
    
    * 3.3 service discovery 3
    
    * 3.3 service discovery 4 notiry when dependency changed

--- cmd/gradingservice/main.go
+++ cmd/gradingservice/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"distributed/grades"
+	"distributed/log"
 	"distributed/registry"
 	"distributed/service"
 	"fmt"
@@ -16,6 +17,8 @@ func main() {
 	r := registry.Registration{
 		ServiceName: registry.GradingService,
 		ServiceURL: serviceAddress,
+		RequiredServices: []registry.ServiceName{registry.LogService},
+		ServiceUpdateURL: serviceAddress + "/services",
 	}
 	ctx, err := service.Start(context.Background(),
 		host,
@@ -25,6 +28,12 @@ func main() {
 	if err != nil {
 		stlog.Fatalf("GradingService start failed with error: %v", err)
 	}
+
+	if logProvider, err := registry.GetProvider(registry.LogService); err == nil {
+		fmt.Printf("Logging service found at: %s\n", logProvider)
+		log.SetClientLogger(logProvider, r.ServiceName)
+	}
+	
 	<- ctx.Done()
 	fmt.Println("Shutting down grading service")
 }
--- registry/server.go
+++ registry/server.go
@@ -1,6 +1,7 @@
 package registry
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -14,7 +15,7 @@ const ServicesURL = "http://localhost" + ServerPort + "/services"
 
 type registry struct {
 	registrations []Registration
-	mutex *sync.Mutex
+	mutex *sync.RWMutex
 }
 
 func (r *registry) add(reg Registration) error {
@@ -22,14 +23,97 @@ func (r *registry) add(reg Registration) error {
 	r.mutex.Lock()
 	r.registrations = append(r.registrations, reg)
 	r.mutex.Unlock()
+	err := r.sendRequiredServices(reg)
+	r.notify(patch{
+		Added: []patchEntry{
+			{
+				Name: reg.ServiceName,
+				URL: reg.ServiceURL,
+			},
+		},
+	})
 	log.Printf("Registrations after add: %v", r.registrations)
+	return err
+}
+
+func (r registry) notify(fullPatch patch) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	for _, reg := range r.registrations {
+		go func(reg Registration) {
+			for _, reqService := range reg.RequiredServices {
+				p := patch{Added: []patchEntry{}, Removed: []patchEntry{}}
+				sendUpdate := false
+				for _, added := range fullPatch.Added {
+					if added.Name == reqService {
+						p.Added = append(p.Added, added)
+						sendUpdate = true
+					}
+				}
+				for _, removed := range fullPatch.Removed {
+					if removed.Name == reqService {
+						p.Removed = append(p.Removed, removed)
+						sendUpdate = true
+					}
+				}
+				if sendUpdate {
+					err := r.sendPatch(p, reg.ServiceUpdateURL)
+					if err != nil {
+						log.Printf("send update patch with error: %v", err)
+					}
+				}
+			}
+		}(reg)
+	}
+}
+
+func (r registry) sendRequiredServices(reg Registration) error {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	var p patch
+	for _, serviceReg := range r.registrations {
+		for _, reqService := range reg.RequiredServices {
+			if serviceReg.ServiceName == reqService {
+				p.Added = append(p.Added, patchEntry{
+					Name: serviceReg.ServiceName,
+					URL: serviceReg.ServiceURL,
+				})
+			}
+		}
+	}
+	err := r.sendPatch(p, reg.ServiceUpdateURL)
+	if err != nil {
+		return err
+	}
 	return nil
 }
+
+func (r registry) sendPatch(p patch, url string) error {
+	d, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+	_, err = http.Post(url, "application/json", bytes.NewReader(d))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 
 func (r *registry) remove (url string) error {
 	log.Printf("Registrations before delete: %v", r.registrations)
 	for i := range reg.registrations {
 		if reg.registrations[i].ServiceURL == url {
+			r.notify(patch{
+				Removed: []patchEntry{
+					{
+						Name: r.registrations[i].ServiceName,
+						URL: r.registrations[i].ServiceURL,
+					},
+				},
+			})
 			r.mutex.Lock()
 			reg.registrations = append(reg.registrations[:i], reg.registrations[i+1:]...)
 			r.mutex.Unlock()
@@ -42,7 +126,7 @@ func (r *registry) remove (url string) error {
 
 var reg = registry{
 	registrations: make([]Registration, 0),
-	mutex: new(sync.Mutex),
+	mutex: new(sync.RWMutex),
 }
 
 type RegistryService struct {}
